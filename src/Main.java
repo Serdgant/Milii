70b58d1a181d821075da029b9e2b06ef2cbcc750
SERG <serdgant66@mail.ru>
2022-06-28 17:56:10 +0300

    add file

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 500;
+        int bonuss = ticket / 20;
+        System.out.println("начисленно бонусов" + bonuss);
+
+    }
+}
